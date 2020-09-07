3719e8f5da2c06c150bfe3538f831c01983209e6
Jeff Kraus <jeffk42@gmail.com>
2020-09-07 00:40:13 -0400

    updates testing text window

--- AuctionHelper.lua
+++ AuctionHelper.lua
@@ -9,6 +9,8 @@ AuctionHelper.name = "AuctionHelper"
 function AuctionHelper:Initialize()
   -- ...but we don't have anything to initialize yet. We'll come back to this.
   AuctionHelper.ConsoleCommands()
+  ScrollListExampleMainWindow:SetHidden(false)
+  -- ScrollListExampleMainWindow:SetText("the message")
 end
  
 -- Then we create an event handler function which will be called when the "addon loaded" event
@@ -21,10 +23,10 @@ function AuctionHelper.OnAddOnLoaded(event, addonName)
 end
 
 local function handleSoldBid(rawName)
-  StartChatInput(string.format("SOLD to %s for MONEY! Congrats!", rawName))
+  StartChatInput(string.format("SOLD to %s for MONEY! Congrats! Please MAIL your winning bid to @AKTT-auction as soon as possible so we can get your item(s) sent out promptly!", rawName))
 end
 
-local function MyShowPlayerContextMenu(playerDatas, rawName)
+local function MyShowPlayerContextMenu(playerData, rawName)
   AddCustomMenuItem("Sold To " .. rawName, function() handleSoldBid(rawName) end)
   ShowMenu()
 end
@@ -77,7 +79,9 @@ function AuctionHelper:ConsoleCommands()
             value = user
             StartChatInput(string.format("%s going twice!", value))
           elseif command == "sold" then
-            StartChatInput(string.format("SOLD to %s for %s! Congrats!", value))
+            StartChatInput(string.format("SOLD to %s for %s! Congrats! Please MAIL your winning bid to @AKTT-auction as soon as possible so we can get your item(s) sent out promptly!", value))
+          elseif command == "" then
+            ScrollListExampleMainWindow:SetHidden(false)
           end
     end
 end
@@ -85,3 +89,51 @@ end
 -- Finally, we'll register our event handler function to be called when the proper event occurs.
 EVENT_MANAGER:RegisterForEvent(AuctionHelper.name, EVENT_ADD_ON_LOADED, AuctionHelper.OnAddOnLoaded)
 SecurePostHook(SharedChatSystem, "ShowPlayerContextMenu", MyShowPlayerContextMenu)
+
+
+
+
+-- test window
+  
+  -- function SLE.MouseEnter(control)
+  --   SLE.UnitList:Row_OnMouseEnter(control)
+  -- end
+  
+  -- function SLE.MouseExit(control)
+  --   SLE.UnitList:Row_OnMouseExit(control)
+  -- end
+  
+  -- function SLE.MouseUp(control, button, upInside)
+  --   local cd = control.data
+  --   d(table.concat( { cd.name, cd.race, cd.class, cd.zone }, " "))
+  -- end
+  
+--  function SLE.TrackUnit()
+--    local targetName = GetUnitName("reticleover")
+--    if targetName == "" then return end
+--    local targetRace = GetUnitRace("reticleover")
+--    local targetClass = GetUnitClass("reticleover")
+--    local targetZone = GetUnitZone("reticleover")
+--    SLE.units[targetName] = {race=tagetRace, class=targetClass, zone=targetZone}
+--    SLE.UnitList:Refresh()
+--  end
+  
+  -- do all this when the addon is loaded
+  -- function .Init(eventCode, addOnName)
+  --   if addOnName ~= "AuctionHelper" then return end
+  
+    -- Event Registration
+    -- EVENT_MANAGER:RegisterForEvent("SLE_RETICLE_TARGET_CHANGED", EVENT_RETICLE_TARGET_CHANGED, SLE.TrackUnit)
+  
+    -- SLE.UnitList = UnitList:New()
+    -- local playerName = GetUnitName("player")
+    -- local playerRace = GetUnitRace("player")
+    -- local playerClass = GetUnitClass("player")
+    -- local playerZone = GetUnitZone("player")
+    -- SLE.units[playerName] = {race=playerRace, class=playerClass, zone=playerZone}
+    -- SLE.UnitList:Refresh()
+  
+    
+  -- end
+  
+  -- EVENT_MANAGER:RegisterForEvent("SLE_Init", EVENT_ADD_ON_LOADED , SLE.Init)
